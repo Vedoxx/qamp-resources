6650c78e7d1598afd662c78279651389718e4983
Medzida Mustafic <extern.mustafic_medzida@allianz.de>
2022-03-22 18:22:58 +0100

    Driver setup and Test Case Implementation

--- src/test/java/placelab/tests/LoginTest.java
+++ src/test/java/placelab/tests/LoginTest.java
@@ -1,9 +1,11 @@
 package placelab.tests;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+import placelab.utilities.WebDriverSetup;
 
 public class LoginTest {
     public WebDriver driver;
@@ -14,6 +16,7 @@ public class LoginTest {
     @BeforeSuite
     public void initDriver() {
 
+        driver = WebDriverSetup.getWebDriver("chrome");
     }
 
     //Actual test case implementation
@@ -21,6 +24,12 @@ public class LoginTest {
     @Test
     public void testLoginPage() {
 
+        //Go to PlaceLab demo app
+        driver.navigate().to(host);
+
+        //Validate that user is redirected to the right page
+        Assert.assertEquals(driver.getCurrentUrl(), host);
+        Assert.assertEquals(driver.getTitle(), "PlaceLab");
 
     }
 
@@ -28,6 +37,8 @@ public class LoginTest {
 
     @AfterSuite
     public void quitDriver() {
+
+        driver.quit();
 
     }
 }
